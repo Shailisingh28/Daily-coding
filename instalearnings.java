90f18ecd1d1878eb5cf880264cbfcbc408f15534
Shailisingh28 <shaili2809@gmail.com>
2026-03-26 19:31:36 +0530

    Value of()

---
+++ instalearnings.java
@@ -0,0 +1,10 @@
+public class instalearnings {
+    public static void main(String[] args) {
+        System.out.println("Java" + 10 + 20);
+        System.out.println(10 + 20 + "Java");
+        int num = 10;
+        float s = Float.valueOf(num);
+        System.out.println(s);
+
+    }
+}
