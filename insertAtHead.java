f73302f74c130fc57d4645c2c6849c3c727ca283
Shailisingh28 <shaili2809@gmail.com>
2026-04-02 01:07:15 +0530

    insert at head

---
+++ insertAtHead.java
@@ -0,0 +1,44 @@
+public class insertAtHead {
+    private Node head = null;
+
+    public class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public Node insertatfirst(int x, Node head) {
+        Node newnNode = new Node(x);
+        if (head == null) {
+            head = newnNode;
+            return head;
+        }
+        newnNode.next = head;
+        head = newnNode;
+        return newnNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String[] args) {
+        insertAtHead list = new insertAtHead();
+        // list.head = list.insertatfirst(1, list.head);
+        // list.head = list.insertatfirst(2, list.head);
+        list.head = list.insertatfirst(3, list.head);
+        list.print();
+    }
+}
