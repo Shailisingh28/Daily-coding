4b2c3ae5b3bac1b03d807317cdb2acab11971e25
Shailisingh28 <shaili2809@gmail.com>
2026-04-02 01:31:11 +0530

    Search

--- insertAtHead.java
+++ insertAtHead.java
@@ -1,5 +1,6 @@
 public class insertAtHead {
     private Node head = null;
+    private int size;
 
     public class Node {
         int val;
@@ -14,18 +15,21 @@ public class insertAtHead {
         Node newnNode = new Node(x);
         if (head == null) {
             head = newnNode;
+            size++;
             return head;
         }
         newnNode.next = head;
         head = newnNode;
+        size++;
         return newnNode;
     }
 
-    public static Node deleteHead(Node head) {
+    public Node deleteHead(Node head) {
         if (head == null) {
             return head;
         }
         head = head.next;
+        size--;
         return head;
     }
 
@@ -42,12 +46,33 @@ public class insertAtHead {
         System.out.print("null");
     }
 
+    public int getsize() {
+        return size;
+    }
+
+    public boolean searchKey(Node head, int key) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.val == key) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         insertAtHead list = new insertAtHead();
         list.head = list.insertatfirst(1, list.head);
         list.head = list.insertatfirst(2, list.head);
         list.head = list.insertatfirst(3, list.head);
-        list.head = list.deleteHead(list.head);
+        list.head = list.insertatfirst(4, list.head);
+
+        // list.head = list.deleteHead(list.head);
         list.print();
+        System.out.println();
+        // int size = list.getsize();
+        // System.out.println(size);
+        System.out.println(list.searchKey(list.head, 00));
     }
 }
