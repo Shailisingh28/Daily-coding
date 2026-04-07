97e8fefc6159f3c5d4dd3cf308cb05d0b35f2676
Shailisingh28 <shaili2809@gmail.com>
2026-04-07 22:35:29 +0530

    deletemiddle

---
+++ deleteMiddle.java
@@ -0,0 +1,53 @@
+public class deleteMiddle {
+    Node head;
+
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public Node deleteMiddle(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        Node pre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = slow.next;
+        return head;
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("empty list");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String[] args) {
+        deleteMiddle obj = new deleteMiddle();
+        obj.head = new Node(1);
+        // obj.head.next = new Node(2);
+        // obj.head.next.next = new Node(3);
+        // obj.head.next.next.next = new Node(4);
+        // obj.head.next.next.next.next = new Node(5);
+        obj.head = obj.deleteMiddle(obj.head);
+        obj.display();
+
+    }
+}
