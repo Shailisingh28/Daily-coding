b04a0a8c957410406e86065dcb780e78182f6ee9
Shailisingh28 <shaili2809@gmail.com>
2026-04-10 10:44:45 +0530

    SortList

--- sortList.java
+++ sortList.java
@@ -1,53 +1,79 @@
-import java.util.Arrays;
-
 public class sortList {
+    private Node head;
 
-    public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
-        return nums;
-    }
+    public static class Node {
+        int val;
+        Node next;
 
-    private void mergeSort(int[] arr, int left, int right) {
-        if (left >= right)
-            return;
-
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
-
-    private void merge(int[] arr, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
-            }
+        Node(int val) {
+            this.val = val;
         }
+    }
 
-        while (i <= mid)
-            temp[k++] = arr[i++];
-        while (j <= right)
-            temp[k++] = arr[j++];
+    public Node sortList(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node mid = middle(head);
+        Node head1 = head;
+        Node head2 = mid.next;
+        mid.next = null;
+        Node left = sortList(head1);
+        Node right = sortList(head2);
+        Node newhead = merge(left, right);
+        return newhead;
+    }
 
-        System.arraycopy(temp, 0, arr, left, temp.length);
+    public Node merge(Node head1, Node head2) {
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        if (head1 != null)
+            temp.next = head1;
+        if (head2 != null)
+            temp.next = head2;
+        return dummy.next;
+    }
+
+    public Node middle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.print("null");
     }
 
     public static void main(String[] args) {
         sortList obj = new sortList();
-
-        int[] nums = { 5, 2, 9, 1, 3 };
-
-        System.out.println("Before Sorting: " + Arrays.toString(nums));
-
-        obj.sortArray(nums);
-
-        System.out.println("After Sorting: " + Arrays.toString(nums));
+        obj.head = new Node(4);
+        obj.head.next = new Node(2);
+        obj.head.next.next = new Node(1);
+        obj.head.next.next.next = new Node(3);
+        obj.head = obj.sortList(obj.head);
+        obj.display();
     }
 }
