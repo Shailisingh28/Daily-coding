141d5e11f94b6424307d0ab8fb8081c705da1d7b
Shailisingh28 <shaili2809@gmail.com>
2026-04-19 23:09:24 +0530

    binary search target finding

---
+++ Helloworld.java
@@ -0,0 +1,24 @@
+public class Helloworld {
+    public static int binaryserach(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int target = 41;
+        System.out.println(binaryserach(arr, target));
+    }
+}
