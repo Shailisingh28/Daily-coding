1b934f98efd05d58ebdd73ed8d0f61ad19a20df6
Shailisingh28 <shaili2809@gmail.com>
2026-04-23 19:34:34 +0530

    subsetwithdup

---
+++ Recursion/subsetsWithDup.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class subsetsWithDup {
+    public static List<List<Integer>> subsetsWithDup(int arr[]) {
+        Arrays.sort(arr);
+        List<List<Integer>> result = new ArrayList<>();
+        generate(arr, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    public static void generate(int arr[], int index, List<Integer> ans, List<List<Integer>> result) {
+
+        result.add(new ArrayList(ans));
+        for (int i = index; i < arr.length; i++) {
+            if (i > index && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            ans.add(arr[i]);
+            generate(arr, i + 1, ans, result);
+            ans.remove(ans.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 3 };
+        System.out.println(subsetsWithDup(arr));
+    }
+}
