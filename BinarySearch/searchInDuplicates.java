43066035708c2e4fc2f763c9537848bd1e7c50bd
Shailisingh28 <shaili2809@gmail.com>
2026-06-10 19:53:39 +0530

    Search in Rotated Sorted Array II (with duplicates)

---
+++ BinarySearch/searchInDuplicates.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+public class searchInDuplicates {
+    public static boolean searchInDuplicates(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+                return true;
+            if (arr[left] == arr[mid] && arr[mid] == arr[right]) {
+                left++;
+                right--;
+            } else if (arr[left] <= arr[mid]) {
+                if (arr[left] <= target && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (arr[mid] < target && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 5, 6, 0, 0, 1, 2 };
+        int target = 0;
+        System.out.println(searchInDuplicates(nums, target));
+    }
+}
