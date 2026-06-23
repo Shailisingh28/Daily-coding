c834d43e5e700e19171b9e2322a279e661a47f12
Shailisingh28 <shaili2809@gmail.com>
2026-06-23 11:01:52 +0530

    Linear search

---
+++ Array/linearSearch.java
@@ -0,0 +1,17 @@
+package Array;
+
+class linearSearch {
+    public static int linearSearch(int arr[], int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 4, 5, 3 };
+        int target = 30;
+        System.out.println(linearSearch(nums, target));
+    }
+}
