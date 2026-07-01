7396877688f815c8de248cd60cc922926125acb3
Shailisingh28 <shaili2809@gmail.com>
2026-07-01 09:52:05 +0530

    Leetcode 81

---
+++ Retake/searchII.java
@@ -0,0 +1,37 @@
+package Retake;
+
+public class searchII {
+    public static boolean searchII(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+                return true;
+            if (arr[left] == arr[mid] && arr[mid] == arr[right]) {
+                left++;
+                right--;
+            } else if (arr[left] <= arr[mid]) {
+                if (arr[left] <= target && target <= arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1, 1, 1 };
+        int target = 0;
+        System.out.println(searchII(nums, target));
+    }
+}
