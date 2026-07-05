3875152da8027d1f3badfca16e44b2e8c0ec3a1d
Shailisingh28 <shaili2809@gmail.com>
2026-07-05 11:33:21 +0530

    Leetcode 154

---
+++ Retake/findMinII.java
@@ -0,0 +1,24 @@
+package Retake;
+
+public class findMinII {
+    public static int findMinII(int arr[]) {
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == arr[right])
+                right--;
+            else if (arr[mid] > arr[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return arr[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 2, 2, 0, 1 };
+        System.out.println(findMinII(nums));
+    }
+}
